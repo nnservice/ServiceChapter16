0d41535f4e4d1c356b1bbf13bc653aac57dcec0e
Namiko <151088424+nnservice@users.noreply.github.com>
2023-11-29 14:35:29 -0700

    Add files via upload

---
+++ ServiceChapter16/R/Flagstaff_Weather.R
@@ -0,0 +1,12 @@
+#' Weather information at Flagstaff’s Pulliam Airport from 1950 to 2019.
+#'
+#' @format a data frame with 22967 observations
+#' \describe{
+#'   \item{DATE}{The date of observation as a POSIX date format.}
+#'   \item{PRECIP}{Daily precipitation given in inches}
+#'   \item{SNOW}{Daily snow given in inches}
+#'   \item{TMAX}{Daily maximum temperature in degrees Farhenheit.}
+#'   \item{TMIN}{Daily minimum temperature in degrees Farhenheit.}
+#' }
+#' @source \url{https://www.ncdc.noaa.gov/cdo-web/search}
+"Flagstaff_Weather"
